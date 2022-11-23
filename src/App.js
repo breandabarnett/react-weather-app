6eaea8fe88a0e2c2ed1222199aa50cdc43afac49
Breanda Barnett <breanda.barnett@gmail.com>
2022-11-23 11:05:24 +0900

    Updated footer url

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ export default function App() {
       <h1>Weather app coming soon...</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://breanda.netlify.app/" target="_blank">
+        <a href="https://breanda.netlify.app/" target="_blank" rel="noreferrer">
           Breanda Barnett
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/breandabarnett/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
