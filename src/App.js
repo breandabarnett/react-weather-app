79bf7cc1643abae48fc655e894a61929f473b3fe
Breanda Barnett <breanda.barnett@gmail.com>
2022-11-23 11:03:23 +0900

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app.</div>;
+  return (
+    <div className="App">
+      <h1>Weather app coming soon...</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://breanda.netlify.app/" target="_blank">
+          Breanda Barnett
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/breandabarnett/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
