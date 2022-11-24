91716e3dc14c5c4b4553498ded7ca0f32fbe8e5f
Breanda Barnett <breanda.barnett@gmail.com>
2022-11-24 11:18:38 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app coming soon...</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://breanda.netlify.app/" target="_blank" rel="noreferrer">
-          Breanda Barnett
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/breandabarnett/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather app coming soon...</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://breanda.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Breanda Barnett
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/breandabarnett/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
