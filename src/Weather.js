4da917d47b961e4d34d829d8f50fb3a6698ed8f0
Breanda Barnett <breanda.barnett@gmail.com>
2022-11-24 13:11:46 +0900

    Added HTML structure

---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type in a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Tokyo, Japan</h1>
+      <ul>
+        <li>Thursday 12:00</li>
+        <li>Clear</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="sun"
+          />
+          19ºC
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
