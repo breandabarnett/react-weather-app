e4b7cc71711297417968a573f2e455593a4882a9
Breanda Barnett <breanda.barnett@gmail.com>
2022-12-06 18:44:53 +0900

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type in a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      date: "Thursday 12:00",
+      description: response.data.condition.description,
+      temperature: response.data.temperature.current,
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png",
+      feelsLike: response.data.temperature.feels_like,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type in a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6 p-0">
+            <div className="clearfix">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">ºC</span>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>Tokyo, Japan</h1>
-      <ul>
-        <li>Thursday 12:00</li>
-        <li>Clear</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="sun"
-              className="float-left"
-            />
-            <span className="temperature">19</span>
-            <span className="unit">ºC</span>
+          <div className="col-6">
+            <ul>
+              <li>Feels Like: {Math.round(weatherData.feelsLike)} ºC</li>
+              <li>
+                Humidity: {weatherData.humidity}
+                {""}%
+              </li>
+              <li>
+                Wind: {Math.round(weatherData.wind)} {""}mph
+              </li>
+            </ul>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13mph</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "2713dc334920f2c6ffb470ta385oa3b9";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
